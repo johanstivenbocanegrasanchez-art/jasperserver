fed96b6a6acf9280e94996422c0cd7cc87dcd584
JOHAN STIVEN BOCANEGRA SANCHEZ <johanstivenbocanegrasanchez@gmail.com>
2026-05-25 14:57:56 -0500

    config inicial: setenv.bat y jasperserver.xml con datasources configurados

---
+++ bin/setenv.bat
@@ -0,0 +1,5 @@
+set JAVA_HOME=C:\Users\JohanS.BocanegraS\AppData\Local\Programs\Eclipse Adoptium\jdk-8.0.492.9-hotspot
+set JRE_HOME=%JAVA_HOME%
+set CATALINA_HOME=C:\tomcat9\apache-tomcat-9.0.118
+set CATALINA_BASE=C:\tomcat9\apache-tomcat-9.0.118
+set JAVA_OPTS=-Xms1024m -Xmx2048m -XX:MaxMetaspaceSize=512m
